de573717ab8d3a17295c4aacf3810dcfefe98280
Reggie <ryoboua@gmail.com>
2018-10-09 20:52:45 -0400

    Add Notes and Edit Account buttons to NavBar

--- frontend/src/navbar/NavBar.js
+++ frontend/src/navbar/NavBar.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Navbar, NavbarBrand, Button } from 'reactstrap'
+import { Link } from 'react-router-dom';
 
-export default ({ name ,showLogOut, logout  }) => (
+
+export default ({ name ,showLogOut, logout }) => (
             <div>
                 <Navbar color="danger" dark expand="md">
                     <NavbarBrand href="/">NotePadApp.</NavbarBrand>
@@ -11,6 +13,12 @@ export default ({ name ,showLogOut, logout  }) => (
                          (
                         <React.Fragment>
                             <h4 className="text-white mx-auto" style={{width: '72%'}} >Hello {name}.</h4>
+                            <Link className="ml-auto" to='/notes'>
+                                <Button type="button" className="ml-auto p-2" outline color="dark" >Notes</Button>
+                            </Link>
+                            <Link className="ml-auto" to='/users/edit'>
+                                <Button type="button" className="ml-auto p-2" outline color="dark" >Edit Account</Button>
+                            </Link>
                             <Button type="button" className="ml-auto p-2" outline color="dark" onClick={logout} >Logout</Button>
                         </React.Fragment>
                          )
