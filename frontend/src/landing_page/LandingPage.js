89efab1c221fd557cd1767943943de17a521d974
Reggie <ryoboua@gmail.com>
2018-10-13 02:04:56 -0400

    Update frontend/src/landing_page/LandingPage.js

--- frontend/src/landing_page/LandingPage.js
+++ frontend/src/landing_page/LandingPage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-//import MediaQuery from 'react-responsive';
-import { Container, Button} from 'reactstrap';
+import { Container, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 //images
