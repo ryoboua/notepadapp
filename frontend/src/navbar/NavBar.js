72dd602cd762c41bb56a2a35f506250949f7265f
Reggie <ryoboua@gmail.com>
2018-10-13 02:05:02 -0400

    Update frontend/src/navbar/NavBar.js

--- frontend/src/navbar/NavBar.js
+++ frontend/src/navbar/NavBar.js
@@ -4,25 +4,25 @@ import { Link } from 'react-router-dom';
 
 
 export default ({ name ,showLogOut, logout }) => (
-            <div>
-                <Navbar color="danger" dark expand="md">
-                    <NavbarBrand href="/">NotePadApp.</NavbarBrand>
-                    {
-                        !showLogOut ? null 
-                        :
-                         (
-                        <React.Fragment>
-                            <h4 className="text-white mx-auto" style={{width: '72%'}} >Hello {name}.</h4>
-                            <Link className="ml-auto" to='/notes'>
-                                <Button type="button" className="ml-auto p-2" outline color="dark" >Notes</Button>
-                            </Link>
-                            <Link className="ml-auto" to='/users/edit'>
-                                <Button type="button" className="ml-auto p-2" outline color="dark" >Edit Account</Button>
-                            </Link>
-                            <Button type="button" className="ml-auto p-2" outline color="dark" onClick={logout} >Logout</Button>
-                        </React.Fragment>
-                         )
-                    }
-                </Navbar>
-            </div>
+        <div>
+            <Navbar color="danger" dark expand="md">
+                <NavbarBrand href="/">NotePadApp.</NavbarBrand>
+                {
+                    !showLogOut ? null 
+                    :
+                        (
+                    <React.Fragment>
+                        <h4 className="text-white mx-auto" style={{ paddingLeft: '160px' }} >Hello {name}.</h4>
+                        <Link className="mx-1" to='/notes'>
+                            <Button type="button" className="" outline color="dark" >Notes</Button>
+                        </Link>
+                        <Link className="mx-1" to='/users/edit'>
+                            <Button type="button" className="" outline color="dark" >Edit Account</Button>
+                        </Link>
+                        <Button type="button" className="mx-1" outline color="dark" onClick={logout} >Logout</Button>
+                    </React.Fragment>
+                        )
+                }
+            </Navbar>
+        </div>
         )
