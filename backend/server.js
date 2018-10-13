c140fdd9adf75a4c14bd1f29d2779d9b4db57452
Reggie <ryoboua@gmail.com>
2018-10-13 02:04:25 -0400

    Update backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,16 @@
 import config from './config/config'
 import app from './config/app.js';
-import mongoose from 'mongoose';
-import User from './models/user.model';
+import mongoose from 'mongoose'
 
-const mongoURI = config.mongo.host;
+const mongoURI = config.mongo.host
 const port = config.port
-mongoose.connect(mongoURI, { useNewUrlParser: true });
-mongoose.connection.once('open', () => {
-    console.log('Connected to Mongo DB')
-  })
-mongoose.connection.on('error', () => {
-    throw new Error(`unable to connect to database: ${mongoURI}`);
-});
-  
-app.listen(port, () => {
-    console.log('Server is up! port ' + port)
-})
 
-const intializedDatabase = async () => {
-    //await User.remove({})
-    // const testUser1 = new User({
-    //     name: 'John',
-    //     email: 'test@gmail.com',
-    //     password: 'temp123',
-    //     notes: [
-    //         { 
-    //             title: 'Note1',
-    //             content: 'long text...',
-    //          },
-    //     ]
-    // })
-    // const user = await testUser1.save()
-}
-intializedDatabase()
+mongoose.connect(mongoURI, { useNewUrlParser: true });
+
+mongoose.connection.once('open', () => console.log('Connected to Mongo DB'))
+
+mongoose.connection.on('error', () => { throw new Error(`unable to connect to database: ${mongoURI}`) })
+  
+app.listen(port, () =>  console.log('Server is up! port ' + port))
 
 module.exports = app
