c11836f4012d08b3250e06d765c4026d1ae02164
Reggie <ryoboua@gmail.com>
2018-10-13 22:15:29 -0400

    Add validation for environment variables

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,14 +1,43 @@
 require('dotenv').config()
+import Joi from 'joi';
 
-const config = {
-    env: '',
-    port: process.env.PORT,
-    mongooseDebug: '',
-    jwtSecret: process.env.JWTSECRET,
-    mongo: {
-      host: process.env.MONGO_HOST,
-      port: ''
-    }
+
+// define validation for all the env vars
+const envVarsSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .allow(['dev', 'prod'])
+    .default('dev'),
+  SERVER_PORT: Joi.number()
+    .default(3030),
+  MONGOOSE_DEBUG: Joi.boolean()
+    .when('NODE_ENV', {
+      is: Joi.string().equal('dev'),
+      then: Joi.boolean().default(true),
+      otherwise: Joi.boolean().default(false)
+    }),
+  JWT_SECRET: Joi.string().required()
+    .description('JWT Secret required to sign'),
+  MONGO_HOST: Joi.string().required()
+    .description('Mongo DB host url'),
+  MONGO_PORT: Joi.number()
+    .default(27017)
+}).unknown()
+  .required();
+
+const { error, value: envVars } = Joi.validate(process.env, envVarsSchema);
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`)
 }
 
-module.exports = config;
+const config = {
+  env: envVars.NODE_ENV,
+  port: envVars.SERVER_PORT,
+  mongooseDebug: envVars.MONGOOSE_DEBUG,
+  jwtSecret: envVars.JWT_SECRET,
+  mongo: {
+    host: envVars.MONGO_HOST,
+    port: envVars.MONGO_PORT
+  }
+}
+
+module.exports = config
