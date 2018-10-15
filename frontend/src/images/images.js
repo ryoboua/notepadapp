2a73cb35c8805ececb24e2da54be01cf81e4ef3c
Reggie <ryoboua@gmail.com>
2018-10-15 16:55:16 -0400

    Update frontend/src/images/images.js

---
+++ frontend/src/images/images.js
@@ -0,0 +1,13 @@
+import noteImageURL from './note.svg'
+import emptyImageURL from './empty.svg'
+
+export default {
+    noteImage: {
+        altText: "Stack of notes",
+        url: noteImageURL
+    },
+    emptyImage: {
+        altText: "Guy holding an empty box",
+        url: emptyImageURL
+    }
+}
