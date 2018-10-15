32ef35374844bbad32306613c8aa743d6cd36bec
Reggie <ryoboua@gmail.com>
2018-10-15 12:51:59 -0400

    Update frontend/src/note_app/NotePad.js

--- frontend/src/note_app/NotePad.js
+++ frontend/src/note_app/NotePad.js
@@ -1,44 +1,10 @@
-import React, { Component } from 'react'
-import { Button } from 'reactstrap'
-import AddNoteForm from './AddNoteForm'
+import React from 'react'
 import Note from './Note'
 
-const AddNote = ({ showForm }) => (
-    <div className="w-100 mb-4" >
-        <Button type="button" className="ml-auto p-2" color="primary" onClick={showForm} >Add Note</Button>
+export default ({ notes, ...props }) => (
+    <div className="dark_template text-center pt-5">
+        <div className="d-flex flex-wrap justify-content-center mb-5 w-75 mx-auto" >
+        { notes.map( note => <Note key={note._id} note={note} {...props} />) }
+        </div>
     </div>
 )
-
-export default class NotePad extends Component {
-    state = {
-        modal: false,
-      }
-
-    toggle = () => {
-      this.setState({
-        modal: !this.state.modal
-      })
-    }
-    render() {
-        return (
-            <div>
-                <AddNoteForm modal={this.state.modal} toggle={this.toggle} createNote={this.props.createNote} />
-                <div className="dark_template text-center pt-5">
-                    <div className="d-flex flex-wrap justify-content-center mb-5 w-75 mx-auto" >
-                        <AddNote showForm={this.toggle} />
-                        { 
-                        this.props.notes
-                        .map( note => <Note 
-                                        key={note._id} 
-                                        note={note} 
-                                        updateNote={this.props.updateNote} 
-                                        deleteNote={this.props.deleteNote}
-                                    /> 
-                            )
-                        }
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
