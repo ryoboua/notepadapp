1c2866dfac949c6adbb0ae38ae79573c11e122df
Reggie <ryoboua@gmail.com>
2018-10-30 17:13:51 -0400

    Update backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
+import fs from 'fs';
+import https from 'https'
 import config from './config/config'
-import app from './config/app.js';
+import app from './config/app.js'
 import mongoose from 'mongoose'
 
 const mongoURI = config.mongo.host
@@ -10,7 +12,15 @@ mongoose.connect(mongoURI, { useNewUrlParser: true });
 mongoose.connection.once('open', () => console.log('Connected to Mongo DB'))
 
 mongoose.connection.on('error', () => { throw new Error(`unable to connect to database: ${mongoURI}`) })
-  
-app.listen(port, () =>  console.log('Server is up! port: ' + port))
+
+const privateKey = fs.readFileSync(process.env.PRIVATE_KEY);
+const certificate = fs.readFileSync(process.env.CERT_CRT);
+
+// https.createServer({
+//     key: privateKey,
+//     cert: certificate
+// }, app).listen(port, () =>  console.log('Server is up! port: ' + port))
+
+app.listen(port, () =>  console.log('Server is up! port: ' + port) )
 
 module.exports = app
