1828d3601ef8732a133a1e064fbed9bc88023165
Reggie <ryoboua@gmail.com>
2018-10-30 22:17:26 -0400

    Update backend/routes/register.route.js

--- backend/routes/register.route.js
+++ backend/routes/register.route.js
@@ -9,10 +9,17 @@ const router = express.Router();
 router.route('/')
 .get((req, res) => res.json({ message: 'Welcome to the registration route' }))
 .post(  
-        validate(paramValidation.createUser),
-        userCtrl.createUser, 
-        authCtrl.issueJWT,
-        userCtrl.createUserResponse
+    validate(paramValidation.createUser),
+    userCtrl.createUser, 
+    authCtrl.issueJWT,
+    userCtrl.createUserResponse
     )
+router.route('/demouser')
+.post(
+    validate(paramValidation.createDemoUser),
+    userCtrl.createDemoUser,
+    authCtrl.issueJWT,
+    userCtrl.createUserResponse
+)
 
 export default router;
