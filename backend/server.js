d4a8ab43ad472abe20c3ba0a84afa0c7d174b6f1
Reggie <ryoboua@gmail.com>
2018-10-30 22:17:32 -0400

    Update backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import mongoose from 'mongoose'
 
 const mongoURI = config.mongo.host
 const port = config.port
+const privateKeyURL = config.privateKey
+const certificateURL = config.certificate
 
 mongoose.connect(mongoURI, { useNewUrlParser: true });
 
@@ -13,14 +15,14 @@ mongoose.connection.once('open', () => console.log('Connected to Mongo DB'))
 
 mongoose.connection.on('error', () => { throw new Error(`unable to connect to database: ${mongoURI}`) })
 
-const privateKey = fs.readFileSync(process.env.PRIVATE_KEY);
-const certificate = fs.readFileSync(process.env.CERT_CRT);
+// const privateKey = fs.readFileSync(privateKeyURL);
+// const certificate = fs.readFileSync(certificateURL);
 
 // https.createServer({
 //     key: privateKey,
 //     cert: certificate
 // }, app).listen(port, () =>  console.log('Server is up! port: ' + port))
 
-app.listen(port, () =>  console.log('Server is up! port: ' + port) )
+app.listen(port, () =>  console.log('Server is up! port: ' + port))
 
 module.exports = app
