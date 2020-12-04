a18cd70157ae15494ade4e79de2a66773f5483b4
Reggie <ryoboua@gmail.com>
2020-12-04 00:01:31 -0500

    Update client.js

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,4 +1,8 @@
-const API_URL = process.env.REACT_APP_API_URL
+let API_URL = 'http://localhost:3030'
+
+if (process.env.NODE_ENV === "production") {
+    API_URL = 'http://notepadapp.whoisreggie.ca/api'
+}
 
 const register = async userCreds => {
     const options = {
@@ -6,12 +10,12 @@ const register = async userCreds => {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-          },
+        },
         body: JSON.stringify(userCreds)
     }
     return fetch(`${API_URL}/register`, options)
-            .then(res => res.json())
-            .catch(e => console.log(e))
+        .then(res => res.json())
+        .catch(e => console.log(e))
 }
 
 const createDemoUser = async name => {
@@ -20,12 +24,12 @@ const createDemoUser = async name => {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
-          },
+        },
         body: JSON.stringify({ name })
     }
     return fetch(`${API_URL}/register/demouser`, options)
-            .then(res => res.json())
-            .catch(e => console.log(e))
+        .then(res => res.json())
+        .catch(e => console.log(e))
 }
 
 const login = async userCreds => {
@@ -34,79 +38,79 @@ const login = async userCreds => {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-          },
+        },
         body: JSON.stringify(userCreds)
     }
     return fetch(`${API_URL}/auth/login`, options)
-            .then(res => res.json())
-            .catch(e => console.log(e))
+        .then(res => res.json())
+        .catch(e => console.log(e))
 }
 
 const updateAcc = async userCreds => {
     console.log(userCreds)
     const options = {
         method: 'POST',
-        headers : {
+        headers: {
             'authorization': `Bearer ${localStorage.npaJWT}`,
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-         },
+        },
         body: JSON.stringify(userCreds)
     }
     return fetch(`${API_URL}/users/${userCreds.id}`, options)
-            .then(res => res.json())
-            .catch(e => console.log(e))
+        .then(res => res.json())
+        .catch(e => console.log(e))
 }
 
 const getUser = async () => {
     const options = {
-        headers : {
+        headers: {
             'authorization': `Bearer ${localStorage.npaJWT}`
         },
     }
     return fetch(`${API_URL}/users`, options)
-            .then(res => res.json())
-            .catch(e => console.log(e))
+        .then(res => res.json())
+        .catch(e => console.log(e))
 }
 
 const createNote = async (userId, note) => {
     const options = {
         method: 'POST',
-        headers : {
+        headers: {
             'authorization': `Bearer ${localStorage.npaJWT}`,
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-         },
+        },
         body: JSON.stringify(note)
     }
     return fetch(`${API_URL}/users/${userId}/notes`, options)
-            .then(res => res.json())
-            .catch(e => console.log(e))    
+        .then(res => res.json())
+        .catch(e => console.log(e))
 }
 
 const updateNote = async (userId, note) => {
     const options = {
         method: 'POST',
-        headers : {
+        headers: {
             'authorization': `Bearer ${localStorage.npaJWT}`,
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-         },
+        },
         body: JSON.stringify(note)
     }
     return fetch(`${API_URL}/users/${userId}/notes/${note._id}`, options)
-            .then(res => res.json())
-            .catch(e => console.log(e))    
+        .then(res => res.json())
+        .catch(e => console.log(e))
 }
 const deleteNote = async (userId, note) => {
     const options = {
         method: 'DELETE',
-        headers : {
+        headers: {
             'authorization': `Bearer ${localStorage.npaJWT}`,
-         },
+        },
     }
     return fetch(`${API_URL}/users/${userId}/notes/${note._id}`, options)
-            .then(res => res.json())
-            .catch(e => console.log(e))    
+        .then(res => res.json())
+        .catch(e => console.log(e))
 }
 export default { register, createDemoUser, login, updateAcc, getUser, createNote, updateNote, deleteNote } 
