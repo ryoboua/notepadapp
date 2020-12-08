a55a76c5ffb72446967d8fd8828d857f3824e157
Reggie <ryoboua@gmail.com>
2020-12-08 01:33:50 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,14 +15,14 @@ mongoose.connection.once('open', () => console.log('Connected to Mongo DB'))
 
 mongoose.connection.on('error', () => { throw new Error(`unable to connect to database: ${mongoURI}`) })
 
-const privateKey = fs.readFileSync(privateKeyURL);
-const certificate = fs.readFileSync(certificateURL);
+// const privateKey = fs.readFileSync(privateKeyURL);
+// const certificate = fs.readFileSync(certificateURL);
 
-https.createServer({
-    key: privateKey,
-    cert: certificate
-}, app).listen(port, () => console.log('Server is up! port: ' + port))
+// https.createServer({
+//     key: privateKey,
+//     cert: certificate
+// }, app).listen(port, () =>  console.log('Server is up! port: ' + port))
 
-//app.listen(port, () => console.log('Server is up! port: ' + port))
+app.listen(port, () => console.log('Server is up! port: ' + port))
 
 module.exports = app
