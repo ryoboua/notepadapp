5ab59c4cef97fdcbf072332dabaa311d58d5b8db
Reggie <ryoboua@gmail.com>
2020-12-09 02:26:43 -0500

    Removed certificates

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,15 @@
-import fs from 'fs';
-import https from 'https'
 import config from './config/config'
 import app from './config/app.js'
 import mongoose from 'mongoose'
 
 const mongoURI = config.mongo.host
 const port = config.port
-const privateKeyURL = config.privateKey
-const certificateURL = config.certificate
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.connection.once('open', () => console.log('Connected to Mongo DB'))
 
 mongoose.connection.on('error', () => { throw new Error(`unable to connect to database: ${mongoURI}`) })
-
-// const privateKey = fs.readFileSync(privateKeyURL);
-// const certificate = fs.readFileSync(certificateURL);
-
-// https.createServer({
-//     key: privateKey,
-//     cert: certificate
-// }, app).listen(port, () =>  console.log('Server is up! port: ' + port))
 
 app.listen(port, () => console.log('Server is up! port: ' + port))
 
